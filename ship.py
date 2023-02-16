02ad9c7aed9368e408347099718044573dc005e7
andrb <borowiak.and@gmail.com>
2023-02-16 15:43:27 +0100

    Added ship_speed attribute to better speed control

--- ship.py
+++ ship.py
@@ -10,12 +10,17 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
+        self.settings = ai_game.settings
+
         # Load ship image and rect
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
         # New ship appear on screen bottom
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Use float number to storage horizontal ship position
+        self.x = float(self.rect.x)
 
         # Option showing ship moving
         self.moving_right = False
@@ -24,10 +29,14 @@ class Ship:
     def update(self):
         """Update ship position depending on key events"""
 
+        # Update ship x position
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Update rect object depending on x value
+        self.rect.x = self.x
 
     def blitme(self):
         """Display ship in actual position"""
