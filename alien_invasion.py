62e3fa2ac10c85ed4fa1f310d3557e60798aead4
andrb <borowiak.and@gmail.com>
2023-02-17 15:56:26 +0100

    Refactor _create_fleet method

--- alien_invasion.py
+++ alien_invasion.py
@@ -89,10 +89,15 @@ class AlienInvasion:
 
         # Create first alien row
         for alien_number in range(number_aliens_x):
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+            self._create_alien(alien_number)
+
+    def _create_alien(self, alien_number):
+        """Create alien and put it in the row"""
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        self.aliens.add(alien)
 
     def _update_screen(self):
         """Refresh object on screen and go to new screen"""
