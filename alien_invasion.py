88c698655b28d7323c6e506e976661f0b9d8aeae
andrb <borowiak.and@gmail.com>
2023-02-20 15:51:46 +0100

    Added speedup after fleet destroyed

--- alien_invasion.py
+++ alien_invasion.py
@@ -84,6 +84,7 @@ class AlienInvasion:
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
             # Reset game stats
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
 
@@ -122,6 +123,7 @@ class AlienInvasion:
             # Remove bullets and create new fleet
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _create_fleet(self):
         """Create alien fleet"""
--- settings.py
+++ settings.py
@@ -10,18 +10,34 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 1.5
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
         # Alien settings
-        self.alien_speed = 0.5
         self.alien_drop_speed = 10
+
+        # Level settings
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings which will be changed during game"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 0.5
+
         # fleet_direction = 1 to move right and -1 to move left
         self.fleet_direction = 1
+
+
+    def increase_speed(self):
+        """Change speed settings"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
